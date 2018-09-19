784c1b6db99432f721c0f3a4dcc0eb787ccd8673
matkeller <mat.keller@gmail.com>
2018-09-19 14:27:17 -0700

    Added authentication for My Account button.

--- MAGBankDemo/ViewController.swift
+++ MAGBankDemo/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import MASFoundation
+import SVProgressHUD
+import SwiftyJSON
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Start MAS
+        MAS.setGrantFlow(MASGrantFlow.password)
+        MAS.start(withDefaultConfiguration: true) { (completed, error) in
+            print ("...Starting MAS!")
+            if (completed == true) {
+                print ("MAS start completed!")
+            } else {
+                print ("MAS   NOT   STARTED.  Errors: ")
+                print (error!)
+            }
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func myAccountButton(_ sender: Any) {
+        
+        print ("My Account button pressed")
+        
+        MAS.getFrom("/maglogin", withParameters: nil, andHeaders: nil, completion: { (response, error) in
+            
+            if (error == nil) {
+                
+                //We have data!
+                SVProgressHUD.dismiss()
+                print("Products response: \(response!["MASResponseInfoBodyInfoKey"]!) ")
+                
+                //Parse JSON
+                print("Try to parse JSON...")
+                let resultJSON : JSON = JSON(response!["MASResponseInfoBodyInfoKey"]!)
+                let message = resultJSON["message"].stringValue
+                let data = ("Message: \(message)")
+                print (data)
+            } else {
+                print ("Error with login: \(error!)")
+            }
+        })
     }
-
+    
 
 }
 
